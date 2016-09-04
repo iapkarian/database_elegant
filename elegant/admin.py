ad40fa30490cd50e05461dc04354e7c6c6c64a27
Irina <apkarianirina@gmail.com>
2016-09-04 21:34:54 +0300

    Make models

--- elegant/admin.py
+++ elegant/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Client, Drug, Medication
+from .models import Procedure_name, Procedure_type, Procedure
+
+admin.site.register(Client)
+admin.site.register(Drug)
+admin.site.register(Medication)
+admin.site.register(Procedure_name)
+admin.site.register(Procedure)
+admin.site.register(Procedure_type)
+
+
+
 
 # Register your models here.
--- elegant/models.py
+++ elegant/models.py
@@ -5,23 +5,28 @@ class Client(models.Model):
     name = models.CharField(max_length=100)
     phone = models.IntegerField()
     birth = models.DateField()
-    note = models.TextField()
-
-class Medication(models.Model):
-    name = models.ForeignKey(Drug.name)
-    data_start = models.DateField()
-    data_end = models.DateField()
+    note = models.TextField(blank=True)
 
 class Drug(models.Model):
     name = models.CharField(max_length=50)
 
-class Procedure(models.Model):
+class Medication(models.Model):
+    drug = models.ForeignKey(Drug)
+    data_start = models.DateField()
+    data_end = models.DateField()
+    client = models.ForeignKey(Client)
 
 class Procedure_name(models.Model):
     name = models.CharField(max_length=50)
 
 class Procedure_type(models.Model):
-    name = models.ForeignKey(Procedure_name.name)
+    procedure_name = models.ForeignKey(Procedure_name)
     prime_cost = models.IntegerField()
     cost = models.IntegerField()
+    consumable = models.IntegerField()
 
+class Procedure(models.Model):
+    client = models.ForeignKey(Client)
+    procedure = models.ForeignKey(Procedure_type)
+    time_start = models.DateTimeField()
+    time_end = models.DateTimeField()
