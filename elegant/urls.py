9b8ef97f612c69666b86357c5ce68e9eb7f747cc
Irina <apkarianirina@gmail.com>
2016-12-13 17:37:51 +0200

    Add main catalog

--- elegant/urls.py
+++ elegant/urls.py
@@ -4,4 +4,5 @@ from elegant import views
 urlpatterns = [
     url(r'^$', views.client_list, name='client_list'),
     url(r'^procedure/$', views.procedure, name='procedure'),
+    url(r'^shugaring/$', views.shugaring, name='procedure')
 ]
--- elegant/views.py
+++ elegant/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from elegant.forms import PostProcedure
 
 def client_list(request):
-    return render(request, 'elegant/base.html')
+    return render(request, 'elegant/main.html')
 
 def procedure(request):
     procedures = PostProcedure
     return render(request, 'elegant/procedure.html', {'procedures': procedures})
+
+def shugaring(request):
+    return render(request, 'elegant/shugaring.html')
