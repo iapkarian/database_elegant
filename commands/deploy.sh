c589878182e99ffda71cb5f1a7dfe032a6c4ab8d
Irina <apkarianirina@gmail.com>
2017-01-03 01:16:39 +0200

    deploy

--- commands/deploy.sh
+++ commands/deploy.sh
@@ -1,4 +1,3 @@
-```
 #!/bin/bash
 set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.."
@@ -8,4 +7,3 @@ find ${DIR} -name '*.pyc' -delete
 find ${DIR} -name '__pycache__' -delete
 python3 ${DIR}/manage.py collectstatic --noinput
 touch ${DIR}/database_elegant/wsgi.py
-```
