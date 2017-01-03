4c59d32ab84d0e73131db137e0aa482ffd7449af
Irina <apkarianirina@gmail.com>
2017-01-04 01:51:37 +0200

    update module shugaring

--- commands/deploy.sh
+++ commands/deploy.sh
@@ -5,5 +5,6 @@ cd ${DIR}
 git pull
 find ${DIR} -name '*.pyc' -delete
 find ${DIR} -name '__pycache__' -delete
+python3 manage.py migrate
 python3 ${DIR}/manage.py collectstatic --noinput
 touch ${DIR}/database_elegant/wsgi.py
