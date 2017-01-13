b2f2ee2fc66023f22e6e0c75838797111871cc3a
Irina <apkarianirina@gmail.com>
2017-01-14 00:36:53 +0200

    fb auto

--- elegant/views.py
+++ elegant/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from elegant.forms import PostProcedure
+from django.shortcuts import render_to_response
+from django.template.context import RequestContext
+
+
+def home(request):
+   context = RequestContext(request, {'request': request, 'user': request.user})
+   return render_to_response('elegant/contact.html', context_instance=context)
 
 
 def client_list(request):
